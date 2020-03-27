f863ff56eb4a0e325a26e732dd20f5840fc2d7e6
rusefi <rusefillc@gmail.com>
2020-03-27 19:15:59 -0400

    BOM

--- cj125_Module/Gerbers/check-bom-stock.bat
+++ cj125_Module/Gerbers/check-bom-stock.bat
@@ -1,6 +1,6 @@
 @rem Usage: bom_jlc.php stock [file.csv] [jlc_part_number_column_name] [designator_column_name] [num_boards] [add_cost_of_manufacturing] [add_cost_of_each_exp_parttype]
 
-set inputFile=Modular_Lowside_R0.1-bom.csv
+set inputFile=R0.1/cj125_Module_R0.1.csv-bom.csv
 set outputFile=bom-stock.txt 
 
 @php -q ../../bom_jlc/bom_jlc.php stock %inputFile% "LCSC Part #" "Designator" 20 9.00 3.00 > %outputFile%
